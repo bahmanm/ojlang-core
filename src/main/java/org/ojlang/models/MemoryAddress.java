69949bbe0b4185cfd240bc9daeb36e731c9147aa
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-09 17:19:42 +0430

    add javadoc

--- src/main/java/org/ojlang/models/MemoryAddress.java
+++ src/main/java/org/ojlang/models/MemoryAddress.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 
 /**
+ * Represents a memory address (e.g. to jump to).
+ * Though this class is a simple wrapper for an `int`, wihtout it the
+ * interpreter/executor couldn't tell if an `int` in memory is simply a number
+ * to push to PS or is a memory address to jump to.
+ *
  * @author Bahman Movaqar [Bahman AT BahmanM.com]
  */
 @Accessors(fluent = true)
