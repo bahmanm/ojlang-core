1f700b0f802dd272e6b6839e2d0c28fa04abf2f8
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-09 23:20:59 +0430

    use SystemState

--- src/main/java/org/ojlang/models/contracts/SystemWord.java
+++ src/main/java/org/ojlang/models/contracts/SystemWord.java
@@ -1,6 +1,11 @@
 package org.ojlang.models.contracts;
 
 /**
+ * A system word.
+ * A word which is implemented in Java (or any other JVM language). A system
+ * word must be registered in Oj runtime in order to be used.
+ *
+ * @see org.ojlang.runtime.SystemWordsRegistry
  * @author Bahman Movaqar [Bahman AT BahmanM.com]
  */
 public interface SystemWord extends Word {
@@ -10,10 +15,13 @@ public interface SystemWord extends Word {
     return true;
   }
 
+  /**
+   * Executes the word mutating the system state.
+   *
+   * @param systat the snapshot of the system state before execution
+   */
   void execute(
-    Memory mem,
-    ParamStack ps,
-    ReturnStack rs
+    SystemState systat
   );
 
 }
