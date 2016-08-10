5ef28995a2f165bc23ed7add69d3d2aa39a10116
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-10 08:49:53 +0430

    remove default impl of name()

--- src/main/java/org/ojlang/runtime/SystemWordsRegistry.java
+++ src/main/java/org/ojlang/runtime/SystemWordsRegistry.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 import org.eclipse.collections.impl.list.mutable.FastList;
 import org.ojlang.models.contracts.SystemWord;
+import org.ojlang.models.syswords.Executor;
+import org.ojlang.models.syswords.Interpreter;
 
 import java.io.Serializable;
 
@@ -17,7 +19,10 @@ public class SystemWordsRegistry implements Serializable {
   final static private long serialVersionUID = 4672296437456620599L;
 
   @Accessors(fluent=true) @Getter
-  final private static FastList<SystemWord> words = FastList.newListWith();
+  final private static FastList<SystemWord> words = FastList.newListWith(
+    new Interpreter(),
+    new Executor()
+  );
 
   static public int size() {
     return words.size();
