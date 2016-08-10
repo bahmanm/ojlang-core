26cd066cf34891d6463c99015f974e3b0c1bcf2a
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-10 09:03:32 +0430

    add sysWords to init params

--- src/main/java/org/ojlang/runtime/Runtime.java
+++ src/main/java/org/ojlang/runtime/Runtime.java
@@ -1,5 +1,6 @@
 package org.ojlang.runtime;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import lombok.val;
@@ -7,12 +8,12 @@ import org.ojlang.models.DictionaryImpl;
 import org.ojlang.models.MemoryImpl;
 import org.ojlang.models.ParamStackImpl;
 import org.ojlang.models.ReturnStackImpl;
-import org.ojlang.models.contracts.Dictionary;
-import org.ojlang.models.contracts.Memory;
-import org.ojlang.models.contracts.ParamStack;
-import org.ojlang.models.contracts.ReturnStack;
+import org.ojlang.models.contracts.*;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Oj runtime.
@@ -25,33 +26,51 @@ public class Runtime implements Serializable {
   final static private long serialVersionUID = 8428732629851491287L;
 
   @Getter
-  final private Dictionary dict = new DictionaryImpl();
+  private int xp;
 
   @Getter
-  final private Memory mem = new MemoryImpl();
+  private Dictionary dict;
 
   @Getter
-  final private ReturnStack rs = new ReturnStackImpl();
+  private Memory mem;
 
   @Getter
-  final private ParamStack ps = new ParamStackImpl();
+  private ReturnStack rs;
 
   @Getter
-  final private int sysMemSize = SystemWordsRegistry.size();
+  private ParamStack ps;
 
+  @Getter
+  private int sysMemSize;
+
+  /**
+   * Private constructor.  Use `init`.
+   */
   private Runtime() {}
 
   /**
-   * Creates a new instance of Oj runtime.
-   * It basically initialises dictionary, return/param stacks with clean states,
+   * Initialises a new instance of Oj runtime.
+   * It basically initialises dictionary, return/param stacks with given states,
    * and puts the XT of system words in memory (starting from 0).
    *
    * @see SystemWordsRegistry
-   * @return
+   * @param systat system execution state
+   * @param sysWords system words
+   * @return a new instance of runtime
    */
-  static public Runtime setup() {
+  static public Runtime init(
+    SystemState systat,
+    List<SystemWord> sysWords
+  ) {
+    assert(systat != null && sysWords != null);
     val runtime = new Runtime();
-    SystemWordsRegistry.words().each(w -> {
+    runtime.mem = systat.mem();
+    runtime.dict = systat.dict();
+    runtime.rs = systat.rs();
+    runtime.ps = systat.ps();
+    runtime.xp = systat.xp();
+    runtime.sysMemSize = sysWords.size();
+    sysWords.forEach(w -> {
       runtime.dict.put(w);
       runtime.mem.add(w.name());
     });
