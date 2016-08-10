0612c67c11a6be620b11280ad38ef29c7c3447a5
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-10 08:50:24 +0430

    add initial empty impl of INTERPRETER

--- src/main/java/org/ojlang/models/syswords/Interpreter.java
+++ src/main/java/org/ojlang/models/syswords/Interpreter.java
@@ -1,19 +1,27 @@
 package org.ojlang.models.syswords;
 
-import org.ojlang.models.contracts.Memory;
-import org.ojlang.models.contracts.ParamStack;
-import org.ojlang.models.contracts.ReturnStack;
+import org.ojlang.models.contracts.SystemState;
 
 /**
+ * Interpreter word.
+ *
  * @author Bahman Movaqar [Bahman AT BahmanM.com]
  */
 public class Interpreter extends AbstractSystemWord {
 
   final static private long serialVersionUID = -1299211002338774173L;
 
-  @Override
-  public void execute(Memory mem, ParamStack ps, ReturnStack rs) {
 
+  @Override
+  public void execute(
+    SystemState systat
+  ) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String name() {
+    return "INTERPRETER";
   }
 
 }
