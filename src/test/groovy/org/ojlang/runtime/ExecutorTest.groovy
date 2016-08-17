8da6ba226926bbc9714cefee4a8d25ce4c484e61
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-17 23:14:17 +0430

    write tests for user-defined words

--- src/test/groovy/org/ojlang/runtime/ExecutorTest.groovy
+++ src/test/groovy/org/ojlang/runtime/ExecutorTest.groovy
@@ -4,6 +4,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.contrib.java.lang.system.ExpectedSystemExit
 import org.junit.contrib.java.lang.system.SystemOutRule
+import org.ojlang.models.WordImpl
 import org.ojlang.models.Xt
 
 /**
@@ -62,6 +63,101 @@ class ExecutorTest {
       .push(10)
       .push(20)
       .push('10 + 20 = ')
+    systat.xp(systat.sysMemSize())
+    systat
+  }
+
+  @Test
+  void testRun_user_word_bye() {
+    sysexitRule.expectSystemExitWithStatus(0)
+    Executor.run(systat_user_word_bye())
+    sysoutRule.log == 'Result = 30'
+  }
+
+  def systat_user_word_bye() {
+    def myWordXt = new Xt(SystemWordsRegistry.words().size() + 4)
+    def systat = Runtime.initClean(SystemWordsRegistry.words()).systat()
+    systat.dict().put(
+      WordImpl.create('MY-WORD', myWordXt)
+    )
+
+    systat.mem().add(
+      10
+    )
+    systat.mem().add(
+      20
+    )
+    systat.mem().add(
+      myWordXt
+    )
+    systat.mem().add(
+      sysWordXt('BYE')
+    )
+    // MY-WORD
+    systat.mem().add(
+      'Result =  '
+    )
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      sysWordXt('ADD')
+    )
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      Return.instance
+    )
+    systat.xp(systat.sysMemSize())
+    systat
+  }
+
+  @Test
+  void testRun_user_word_user_word_bye() {
+    sysexitRule.expectSystemExitWithStatus(0)
+    Executor.run(systat_user_word_user_word_bye())
+    sysoutRule.log == 'INSIDE INSIDE 12'
+  }
+
+  def systat_user_word_user_word_bye() {
+    def myWordXt = new Xt(SystemWordsRegistry.words().size() + 5)
+    def systat = Runtime.initClean(SystemWordsRegistry.words()).systat()
+    systat.dict().put(
+      WordImpl.create('MY-WORD', myWordXt)
+    )
+
+    systat.mem().add(
+      10
+    )
+    systat.mem().add(
+      myWordXt
+    )
+    systat.mem().add(
+      myWordXt
+    )
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      sysWordXt('BYE')
+    )
+    // MY-WORD
+    systat.mem().add(
+      'INSIDE '
+    )
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      1
+    )
+    systat.mem().add(
+      sysWordXt('ADD')
+    )
+    systat.mem().add(
+      Return.instance
+    )
     systat.xp(systat.sysMemSize())
     systat
   }
