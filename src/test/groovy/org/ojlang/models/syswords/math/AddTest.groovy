349e76b09db004b45f0b759ff4d645cd6a170db4
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-17 09:17:20 +0430

    use TestUtils

--- src/test/groovy/org/ojlang/models/syswords/math/AddTest.groovy
+++ src/test/groovy/org/ojlang/models/syswords/math/AddTest.groovy
@@ -1,11 +1,8 @@
 package org.ojlang.models.syswords.math
 
 import org.junit.Test
-import org.ojlang.models.DictionaryImpl
-import org.ojlang.models.MemoryImpl
-import org.ojlang.models.ParamStackImpl
-import org.ojlang.models.ReturnStackImpl
-import org.ojlang.models.SystemStateImpl
+
+import static org.ojlang.TestUtils.freshSystat
 
 /**
  * @author Bahman Movaqar [Bahman AT BahmanM.com]
@@ -14,14 +11,7 @@ class AddTest {
 
   @Test
   void execute() {
-    def systat = SystemStateImpl.create(
-      new MemoryImpl(),
-      new DictionaryImpl(),
-      new ReturnStackImpl(),
-      new ParamStackImpl(),
-      0,
-      0
-    )
+    def systat = freshSystat()
     systat.ps().push(8 as Integer)
     systat.ps().push(9 as Integer)
     def w = new Add()
