5c65edbc879d695cc2d902304477a7742f96db3d
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-17 22:58:33 +0430

    take into account the system words

--- src/main/java/org/ojlang/runtime/Executor.java
+++ src/main/java/org/ojlang/runtime/Executor.java
@@ -2,6 +2,7 @@ package org.ojlang.runtime;
 
 import org.ojlang.models.Xt;
 import org.ojlang.models.contracts.SystemState;
+import org.ojlang.models.contracts.SystemWord;
 
 /**
  * The executor infinite loop.
@@ -16,24 +17,58 @@ public class Executor {
   ) {
     while (true) {
       Object obj = systat.mem().get(systat.xp());
-      if (obj.getClass().equals(Xt.class)) {
-        systat.rs().push(
-          systat.xp() + 1
-        );
-        systat.xp(
-          ((Xt)obj).value()
-        );
-      } else if (obj.getClass().equals(Return.class)) {
-        systat.xp(
-          systat.rs().pop()
-        );
-      } else {
-        systat.ps().push(obj);
-        systat.xp(
-          systat.xp() + 1
-        );
-      }
+      if (obj.getClass().equals(Xt.class))
+        executeWord(systat, (Xt) obj);
+      else if (obj.getClass().equals(Return.class))
+        executeReturn(systat, (Return) obj);
+      else
+        executeObject(systat, obj);
     }
+  }
+
+  static private SystemState
+  executeWord(
+    SystemState systat, Xt xt
+  ) {
+    if (xt.value() < systat.sysMemSize()) {
+      String wordName = (String) systat.mem().get(
+        xt.value()
+      );
+      SystemWord word = (SystemWord) systat.dict().get(wordName);
+      word.execute(systat);
+      systat.xp(
+        systat.xp() + 1
+      );
+    } else {
+      systat.rs().push(
+        systat.xp() + 1
+      );
+      systat.xp(
+        xt.value()
+      );
+    }
+    return systat;
+  }
+
+  static private SystemState
+  executeReturn(
+    SystemState systat, Return ret
+  ) {
+    systat.xp(
+      systat.rs().pop()
+    );
+    return systat;
+  }
+
+  static private SystemState
+  executeObject(
+    SystemState systat, Object obj
+  ) {
+    systat.ps().push(obj);
+    systat.xp(
+      systat.xp() + 1
+    );
+    return systat;
   }
 
 }
--- src/test/groovy/org/ojlang/runtime/ExecutorTest.groovy
+++ src/test/groovy/org/ojlang/runtime/ExecutorTest.groovy
@@ -1,8 +1,77 @@
-import static org.junit.Assert.*;
+package org.ojlang.runtime
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.contrib.java.lang.system.ExpectedSystemExit
+import org.junit.contrib.java.lang.system.SystemOutRule
+import org.ojlang.models.Xt
 
 /**
  * @author Bahman Movaqar [Bahman AT BahmanM.com]
  */
-public class ExecutorTest {
+class ExecutorTest {
+
+  @Rule
+  public SystemOutRule sysoutRule = new SystemOutRule().enableLog()
+
+  @Rule
+  public ExpectedSystemExit sysexitRule = ExpectedSystemExit.none()
+
+  @Test
+  void testRun_puts_bye() {
+    sysexitRule.expectSystemExitWithStatus(0)
+    Executor.run(systat_puts_bye())
+    sysoutRule.log == 'Hello, world'
+  }
+
+  def systat_puts_bye() {
+    def systat = Runtime.initClean(SystemWordsRegistry.words()).systat()
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      sysWordXt('BYE')
+    )
+    systat.ps().push('Hello, world')
+    systat.xp(systat.sysMemSize())
+    systat
+  }
+
+  @Test
+  void testRun_add_puts_bye() {
+    sysexitRule.expectSystemExitWithStatus(0)
+    Executor.run(systat_add_puts_bye())
+    sysoutRule.log == '10 + 20 = 30'
+  }
+
+  def systat_add_puts_bye() {
+    def systat = Runtime.initClean(SystemWordsRegistry.words()).systat()
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      sysWordXt('ADD')
+    )
+    systat.mem().add(
+      sysWordXt('PUTS')
+    )
+    systat.mem().add(
+      sysWordXt('BYE')
+    )
+    systat.ps()
+      .push(10)
+      .push(20)
+      .push('10 + 20 = ')
+    systat.xp(systat.sysMemSize())
+    systat
+  }
+
+  def sysWordXt(String name) {
+    new Xt(
+      SystemWordsRegistry.words().findIndexOf {
+        it.name() == name
+      }
+    )
+  }
 
 }
