db6440b781f7977b772ab9061ccf82e0800e4257
Bahman Movaqar <Bahman@BahmanM.com>
2016-08-17 13:53:43 +0430

    add Executor (initial impl)

--- src/main/java/org/ojlang/runtime/Executor.java
+++ src/main/java/org/ojlang/runtime/Executor.java
@@ -1,7 +1,39 @@
 package org.ojlang.runtime;
 
+import org.ojlang.models.Xt;
+import org.ojlang.models.contracts.SystemState;
+
 /**
+ * The executor infinite loop.
+ *
  * @author Bahman Movaqar [Bahman AT BahmanM.com]
  */
 public class Executor {
+
+  static public void
+  run(
+    SystemState systat
+  ) {
+    while (true) {
+      Object obj = systat.mem().get(systat.xp());
+      if (obj.getClass().equals(Xt.class)) {
+        systat.rs().push(
+          systat.xp() + 1
+        );
+        systat.xp(
+          ((Xt)obj).value()
+        );
+      } else if (obj.getClass().equals(Return.class)) {
+        systat.xp(
+          systat.rs().pop()
+        );
+      } else {
+        systat.ps().push(obj);
+        systat.xp(
+          systat.xp() + 1
+        );
+      }
+    }
+  }
+
 }
